5de92c2e7af7e0630fc6e10e4d911ab77d79f9a0
Rahul Dani <32138368+15dani1@users.noreply.github.com>
2019-05-04 19:35:14 -0400

    Convered to .py file and added logs folder to use with TensorBoard

---
+++ Deep Learning with Cat and Dog/Cat_Dog Image Recog.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.callbacks import TensorBoard
+import pickle
+import time
+
+NAME = "Cats-vs-Dogs-64x2-{}".format(int(time.time()))
+
+tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
+
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
+sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+
+x = pickle.load(open("x.pickle", "rb"))
+y = pickle.load(open("y.pickle", "rb"))
+
+x = x/255.0
+
+model = Sequential()
+
+model.add(Conv2D(64, (3,3), input_shape= x.shape[1:]))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(64, (3,3), input_shape= x.shape[1:]))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Flatten())
+
+model.add(Dense(64))
+model.add(Activation("relu"))
+
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss="binary_crossentropy",
+             optimizer="adam",
+             metrics=['accuracy'])
+
+model.fit(x, y, batch_size=32, epochs=10, validation_split=0.1, callbacks=[tensorboard])
